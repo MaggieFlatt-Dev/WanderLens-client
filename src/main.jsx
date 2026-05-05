7ce4548086f90f6b142e4605d4cf7b0432d84fa4
Maggie Flatt <maggieflatt19@gmail.com>
2026-05-04 20:54:37 -0500

    changed React Strict Mode to BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ApplicationViews from './components/ApplicationViews.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <ApplicationViews />
-  </React.StrictMode>,
+  </BrowserRouter>
 )
