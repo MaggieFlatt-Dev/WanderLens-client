e0195af21df97e3d501f05aee3c2f3927f0c4ca3
Maggie Flatt <maggieflatt19@gmail.com>
2026-05-06 19:59:56 -0500

    added authorization to fetch calls

--- src/components/services/categoryServices.jsx
+++ src/components/services/categoryServices.jsx
@@ -1,3 +1,7 @@
 export const getCategories = () => {
-    return fetch("http://localhost:8000/api/categories").then(res => res.json())
+    return fetch("http://localhost:8000/api/categories", {
+        headers: {
+            Authorization: "Token " + JSON.parse(localStorage.getItem("WanderLens_token")).token,
+        },
+    }).then((res) => res.json())
 }
--- src/components/services/tripServices.jsx
+++ src/components/services/tripServices.jsx
@@ -2,7 +2,8 @@ export const createTrip = (trip) => {
     return fetch("http://localhost:8000/api/trips", {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+             Authorization: "Token " + JSON.parse(localStorage.getItem("WanderLens_token")).token,
         },
         body: JSON.stringify(trip)
     }).then(res => res.json())
