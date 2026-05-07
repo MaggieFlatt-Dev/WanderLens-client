8cf75aa8df124c783872361cf9f935c53b895862
Maggie Flatt <maggieflatt19@gmail.com>
2026-05-06 20:03:34 -0500

    added logic for tripForm modal

--- src/components/trips/TripList.jsx
+++ src/components/trips/TripList.jsx
@@ -1,5 +1,16 @@
+import { useState } from 'react';
+import { TripForm } from './TripForm';
+
 export const TripList = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   return (
-    <div> Trip List </div>
-  )
- }
+    <div>
+      <h2>Trip List</h2>
+      <button className="border border-gray-300 rounded px-3 py-2 text-sm" onClick={() => setModalIsOpen(true)}>
+      + Create Trip
+      <TripForm isOpen={modalIsOpen} onClose={() => setModalIsOpen(false)} />
+      </button>
+    </div>
+  );
+};
