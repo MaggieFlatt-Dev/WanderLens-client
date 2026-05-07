6b423fa4e507d235cd6006c56900170fb29a8a03
Maggie Flatt <maggieflatt19@gmail.com>
2026-05-07 12:10:20 -0500

    added color picker and fixed to absolute so it floats

--- src/components/trips/TripForm.jsx
+++ src/components/trips/TripForm.jsx
@@ -3,10 +3,13 @@ import ReactModal from "react-modal";
 import { getTripTypes } from "../services/tripTypeServices";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { SwatchesPicker } from "react-color";
 
 export const TripForm = ({ isOpen, onClose }) => {
   const [tripTypes, setTripTypes] = useState([]);
   const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedColor, setSelectedColor] = useState();
+  const [colorPickerIsOpen, setColorPickerIsOpen] = useState(false);
 
   useEffect(() => {
     getTripTypes().then(setTripTypes);
@@ -14,6 +17,10 @@ export const TripForm = ({ isOpen, onClose }) => {
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
+  };
+
+  const handleColorChange = (color) => {
+    setSelectedColor(color);
   };
 
   return (
@@ -30,7 +37,7 @@ export const TripForm = ({ isOpen, onClose }) => {
             className="text-sm font-medium text-gray-700"
             htmlFor="inputTitle"
           >
-            Trip Name*
+            Trip Name <span className="text-red-500">*</span>
           </label>
           <input
             type="text"
@@ -52,7 +59,6 @@ export const TripForm = ({ isOpen, onClose }) => {
             id="inputDescription"
             className="border border-gray-300 rounded px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
             placeholder="Trip Description"
-            required
           />
         </fieldset>
         <fieldset className="flex flex-col gap-1 border-0 p-0">
@@ -60,7 +66,7 @@ export const TripForm = ({ isOpen, onClose }) => {
             className="text-sm font-medium text-gray-700"
             htmlFor="inputTripType"
           >
-            Trip Type*
+            Trip Type <span className="text-red-500">*</span>
           </label>
           <select
             id="inputTripType"
@@ -75,27 +81,49 @@ export const TripForm = ({ isOpen, onClose }) => {
             ))}
           </select>
         </fieldset>
-        <fieldset>
-          <label
-            className="text-sm font-medium text-gray-700"
-            htmlFor="inputStartDate"
-          >
-            Select Start Date:
-          </label>
-          <DatePicker
-            className="ml-2 border border-gray-300 rounded"
-            selected={selectedDate}
-            onChange={handleDateChange}
-            dateFormat="MM/dd/yyyy"
-          />
-        </fieldset>
-        <fieldset>
-          <label className="text-sm font-medium text-gray-700"
-            htmlFor="inputColorPicker">
-            Color: 
-          </label>
-        </fieldset>
-        <div className="text-sm">* required fields</div>
+        <div className="flex flex-row items-center items-end gap-x-30">
+          <fieldset className="relative">
+            <label
+              className="text-sm font-medium text-gray-700"
+              htmlFor="inputColorPicker"
+            >
+              Color: <span className="text-red-500">*</span>
+            </label>
+            <button
+              type="button"
+              onClick={() => setColorPickerIsOpen(!colorPickerIsOpen)}
+              className="ml-2 w-8 h-8 rounded border border-gray-300"
+              style={{ backgroundColor: selectedColor?.hex ?? "#ffffff" }}
+              required
+            />
+            {colorPickerIsOpen && (
+              <div className="absolute z-10">
+              <SwatchesPicker
+                color={selectedColor}
+                onChange={(color) => {
+                  handleColorChange(color);
+                  setColorPickerIsOpen(false);
+                }}
+                />
+                </div>
+            )}
+          </fieldset>
+          <fieldset>
+            <label
+              className="text-sm font-medium text-gray-700"
+              htmlFor="inputStartDate"
+            >
+              Select Start Date: <span className="text-red-500">*</span>
+            </label>
+            <DatePicker
+              className="ml-2 border border-gray-300 rounded w-25"
+              selected={selectedDate}
+              onChange={handleDateChange}
+              dateFormat="MM/dd/yyyy"
+              required
+            />
+          </fieldset>
+        </div>
         <button
           type="submit"
           className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
