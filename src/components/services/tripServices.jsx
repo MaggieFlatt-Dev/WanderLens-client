20ed77fd040550c57b0fb7a1f6cf71a999e4abba
Maggie Flatt <maggieflatt19@gmail.com>
2026-05-07 19:08:36 -0500

    created getTrips fetch call to get all users trips

--- src/components/services/tripServices.jsx
+++ src/components/services/tripServices.jsx
@@ -1,3 +1,11 @@
+export const getTrips = () => {
+    return fetch("http://localhost:8000/api/trips", {
+          headers: {
+            Authorization: "Token " + JSON.parse(localStorage.getItem("WanderLens_token")).token,
+        },
+    }).then((res) => res.json())
+ }
+
 export const createTrip = (newTrip) => {
     return fetch("http://localhost:8000/api/trips", {
         method: "POST",
