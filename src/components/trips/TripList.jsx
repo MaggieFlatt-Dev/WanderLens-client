1418f82cb6b79846c0159720f03768508a0beacf
Maggie Flatt <maggieflatt19@gmail.com>
2026-05-07 19:09:23 -0500

    added .map to show users trips

--- src/components/trips/TripList.jsx
+++ src/components/trips/TripList.jsx
@@ -1,16 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { TripForm } from './TripForm';
+import { getTrips } from '../services/tripServices';
 
 export const TripList = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [trips, setTrips] = useState([]);
+
+  const fetchTrips = () => {
+    getTrips().then(setTrips)
+  }
+
+  useEffect(() => {
+    fetchTrips()
+  }, [])
 
   return (
     <div>
-      <h2>Trip List</h2>
+      <div className="flex flex-row ">
+      <h2>My Trips</h2>
       <button className="border border-gray-300 rounded px-3 py-2 text-sm" onClick={() => setModalIsOpen(true)}>
-      + Create Trip
+      + New Trip
       </button>
-      <TripForm isOpen={modalIsOpen} onClose={() => setModalIsOpen(false)} />
+      <TripForm isOpen={modalIsOpen} onClose={() => setModalIsOpen(false)} onTripCreated={fetchTrips} />
+        </div>
+      <div>
+         {trips.map((trip) => (
+              <div key={trip.id} value={trip.id}>
+             {trip.name}
+             {trip.description}
+              </div>
+            ))}
+      </div>
     </div>
   );
 };
