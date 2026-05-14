9f8d9ab06cdb306b818fac2f7c6f0e0eac7289c8
Maggie Flatt <maggieflatt19@gmail.com>
2026-05-14 14:25:29 -0500

    created deleteStop function

--- src/components/services/stopServices.jsx
+++ src/components/services/stopServices.jsx
@@ -16,3 +16,14 @@ export const getStopById = (id) => {
         }, 
     }).then((res => res.json()))
 }
+
+export const deleteStop = (id) => {
+    return fetch(`http://localhost:8000/api/stops/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: "Token " + JSON.parse(localStorage.getItem("WanderLens_token")).token,
+        },
+        body: JSON.stringify(id)
+    })
+ }
